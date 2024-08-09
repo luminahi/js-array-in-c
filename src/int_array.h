5e048342d016d04402d5fdf6eb1592f3ca49948f
Alex Lima <delimaferreiraalex@gmail.com>
2024-08-09 17:04:23 -0300

    ref: remove unnecessary function declarations

--- src/int_array.h
+++ src/int_array.h
@@ -1,6 +1,8 @@
 #ifndef _INT_ARRAY_H
 #define _INT_ARRAY_H
 
+#include <stdbool.h>
+
 typedef struct IntArray IntArray;
 
 struct IntArray {
@@ -8,20 +10,12 @@ struct IntArray {
   int length;
   void (*for_each)(IntArray*, void (*)(int));
   IntArray* (*map)(IntArray*, int (*)(int));
+  IntArray* (*filter)(IntArray*, bool (*)(int));
+  char* (*join)(IntArray*, const char*);
   void (*fill)(IntArray*, int);
   int (*at)(IntArray*, int);
   void (*destroy)(IntArray*);
 };
-
-void for_each(IntArray*, void (*)(int));
-
-IntArray* map(IntArray*, int (*)(int));
-
-void fill(IntArray*, int);
-
-int at(IntArray*, int);
-
-void destroy(IntArray*);
 
 IntArray* new_int_array(int);
 
